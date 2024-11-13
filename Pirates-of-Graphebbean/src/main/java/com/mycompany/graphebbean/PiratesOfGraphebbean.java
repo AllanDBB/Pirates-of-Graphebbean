f74a022d417faef94ea3cdbb1db788cce85e93d0
IanUgaldeTec <167656913+IanUgaldeTec@users.noreply.github.com>
2024-11-13 15:40:14 -0600

    without folders

--- Pirates-of-Graphebbean/src/main/java/com/mycompany/graphebbean/PiratesOfGraphebbean.java
+++ Pirates-of-Graphebbean/src/main/java/com/mycompany/graphebbean/PiratesOfGraphebbean.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.pirates.of.graphebbean;
+package com.mycompany.graphebbean;
 
 /**
  *
